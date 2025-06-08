2b9f8eb724334a2f99fb6f1c8295841fcd44b7eb
komal-bendresh <komalbendresh1134@gmail.com>
2025-06-08 13:42:25 +0530

    Added authRoutes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/database')
+const cookieParser = require('cookie-parser');
 
 // Load environment variables
 dotenv.config();
@@ -12,6 +13,11 @@ const PORT = process.env.PORT || 5000;
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
+
+
+const authRoutes = require('./routes/authRoutes');
+app.use('/api/auth', authRoutes);
 
 connectDB();
 
