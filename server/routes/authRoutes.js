251955f999ea94db7f4985e4eb6010e832f66e15
komal-bendresh <komalbendresh1134@gmail.com>
2025-06-08 13:43:37 +0530

    Added signup,login,verifyOtp routes

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+// Import controller functions
+const {registerUser, loginUser, verifyOtp } = require('../controllers/authController');
+
+// Routes
+router.post('/signup',registerUser)
+router.post('/login', loginUser);        // Step 1: Login & send OTP
+router.post('/verify-otp', verifyOtp);   // Step 2: Verify OTP & complete login
+
+module.exports = router;
