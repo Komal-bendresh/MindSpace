e36bde9698ee98efb97edd17037593ad38430848
komal-bendresh <komalbendresh1134@gmail.com>
2025-06-13 23:13:16 +0530

    Added unsubcribe route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 // Import controller functions
-const {registerUser, loginUser, verifyOtp ,resendOtp} = require('../controllers/authController');
+const {registerUser, loginUser, verifyOtp ,resendOtp ,unSubscribe} = require('../controllers/authController');
 
 
 // Routes
@@ -11,6 +11,5 @@ router.post('/login', loginUser);        // Step 1: Login & send OTP
 router.post('/verify-otp', verifyOtp);   // Step 2: Verify OTP & complete login
 router.post("/resend-otp", resendOtp);
 
-
-
+router.get('/unsubscribe/:userId',unSubscribe )
 module.exports = router;
