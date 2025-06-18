67fa99313261c4750d86b98e0d5c29b2b0b21e04
komal-bendresh <komalbendresh1134@gmail.com>
2025-06-18 19:59:11 +0530

    Add Layout and nested routes in it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,18 @@ import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import VerifyOtp from "./pages/VerifyOtp";
 import Home from "./pages/Home";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/verify-otp" element={<VerifyOtp />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="login" element={<Login />} />
+          <Route path="verify-otp" element={<VerifyOtp />} />
+        </Route>
       </Routes>
     </Router>
   );
