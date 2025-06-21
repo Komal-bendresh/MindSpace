8cd7ca40fb4e3201023d16757fd4a71a816eabf9
komal-bendresh <komalbendresh1134@gmail.com>
2025-06-21 23:36:39 +0530

    added journal route in nav bar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import DarkModeToggle from "./DarkModeToggle"; 
 
 const Header = () => {
   return (
-    <header className="fixed top-0 w-full bg-blue-600 text-white shadow z-50">
+    <header className="fixed top-0 w-full bg-blue-600 dark:bg-zinc-900 text-white shadow z-50">
       <div className="max-w-6xl mx-auto px-4 py-3 flex justify-between items-center">
+    
         <Link to="/" className="text-xl font-bold">MindSpace</Link>
 
-        <nav className="space-x-4">
-          <Link to="/" className="hover:underline">Home</Link>
-          <Link to="/login" className="hover:underline">Login</Link>
-          <Link to="/signup" className="hover:underline">Signup</Link>
-        </nav>
+        <div className="flex items-center gap-4">
+          <nav className="space-x-4">
+            <Link to="/" className="hover:underline">Home</Link>
+            <Link to="/login" className="hover:underline">Login</Link>
+            <Link to="/signup" className="hover:underline">Signup</Link>
+            <Link to="/journal" className="hover:underline">Journal</Link>
+          </nav>
+
+          <DarkModeToggle />
+        </div>
       </div>
     </header>
   );
