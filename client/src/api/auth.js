ee5f6775c0810abf4a16e4ec3d0e1150628f7647
komal-bendresh <komalbendresh1134@gmail.com>
2025-06-25 23:56:54 +0530

    fixed cors eroe

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -12,11 +12,18 @@ export const loginUser = async (data) => {
   return await axios.post('/api/auth/login', data);
 };
 
-export const createJournalEntry = async (data) => {
-  return await axios.post('/api/journal', data);
+export const createJournalEntry = async ({ mood, text }) => {
+  const res = await axios.post(
+    "/api/journal",
+    { mood, text },
+    { withCredentials: true } 
+  );
+  return res.data.entry;
 };
 
 export const getJournalEntries = async () => {
-  const res = await axios.get("/api/journal/my-entries");
-  return res.data.entries; 
+  const res = await axios.get("/api/journal/my-entries", {
+    withCredentials: true, 
+  });
+  return res.data.entries;
 };
