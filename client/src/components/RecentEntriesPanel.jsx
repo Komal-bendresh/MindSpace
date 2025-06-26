00edd248e4b7f426e2a03920d68badae2eba892c
komal-bendresh <komalbendresh1134@gmail.com>
2025-06-26 23:06:44 +0530

    added onEdit,onDelete  props

--- client/src/components/RecentEntriesPanel.jsx
+++ client/src/components/RecentEntriesPanel.jsx
@@ -1,15 +1,20 @@
 import React, { useState } from "react";
-import { getJournalEntries } from "../api/auth"
+import { getJournalEntries } from "../api/auth";
+import {deleteJournalEntry} from '../api/auth';
+import {updateJournalEntry } from '../api/auth';
+
+
 
 const moods = [
   { label: "Happy", value: "happy", emoji: "😄" },
   { label: "Sad", value: "sad", emoji: "😢" },
   { label: "Angry", value: "angry", emoji: "😠" },
   { label: "Relaxed", value: "relaxed", emoji: "😌" },
-  { label: "Neutral", value: "neutral", emoji: "😐" },
+  { label: "Neutral", value: "neutral", emoji: "😐" }, 
 ];
 
-const RecentEntriesPanel = ({  entries = []  }) => {
+
+const RecentEntriesPanel = ({  entries = [],onEdit,onDelete  }) => {
 
  
 
@@ -26,18 +31,46 @@ const RecentEntriesPanel = ({  entries = []  }) => {
               className="p-3 bg-gray-100 dark:bg-zinc-800 rounded-lg shadow text-sm"
             >
               <div className="flex items-center justify-between mb-1">
-                <span className="text-2xl">
+                <div className="flex items-center justify-between mb-1 gap-2 text-violet-400">
+                  <span className="text-2xl">
                   {moods.find((m) => m.value === entry.mood)?.emoji || "📝"}
                 </span>
+                <span className="text-xl line-clamp-1">
+                  {entry.title? "" : new Date(entry.createdAt).toLocaleDateString()}
+                {entry.title?.length > 20 ? entry.title.slice(0, 20) + "..." : entry.title}
+                </span>
+
+                </div>
+                
                 <span className="text-xs text-gray-400">
                   {new Date(entry.createdAt).toLocaleDateString()}
                 </span>
               </div>
-              <p className="line-clamp-2">{entry.text}</p>
+            <div className="flex justify-between">
+               <p className="line-clamp-2">{entry.text}</p>
+           <div className="flex justify-end gap-2 mt-2">
+          <button
+            onClick={() => onEdit(entry)}
+            className="text-blue-500 text-xs hover:underline"
+          >
+            Edit
+          </button>
+          <button
+            onClick={() => onDelete(entry._id)}
+            className="text-red-500 text-xs hover:underline"
+          >
+            Delete
+          </button>
+        </div>
+            </div>
+             
             </li>
           ))}
+          
         </ul>
+        
       )}
+      
     </div>
   );
 };
