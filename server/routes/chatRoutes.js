8d36b5d8fefc6acf5ce69214b75d03563bbe877f
komal-bendresh <komalbendresh1134@gmail.com>
2025-07-02 09:53:08 +0530

    added deleteChat,clearChatHistory,startNewChat routes

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { chatWithAI ,getChatHistory} = require("../controllers/chatController");
+const { chatWithAI, getChatHistory, clearChatHistory, startNewChat,deleteChat } = require("../controllers/chatController");
 const authMiddleware = require("../middleware/authMiddleware");
 
-router.post("/ai", authMiddleware, chatWithAI);
+router.post("/chat/:id", authMiddleware, chatWithAI);
+router.post("/", authMiddleware, startNewChat);
 router.get("/chat-history", authMiddleware, getChatHistory);
-
+router.delete("/chat-clear/:id", authMiddleware, clearChatHistory);
+router.delete('/delete/:id', authMiddleware, deleteChat);
 module.exports = router;
