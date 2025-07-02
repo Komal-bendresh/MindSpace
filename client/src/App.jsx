da26c6c0db01d0e20ba38dce769a32fae11b4a26
komal-bendresh <komalbendresh1134@gmail.com>
2025-07-02 22:43:02 +0530

    added admin route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,4 @@
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Signup from "./pages/Signup";
-// import Login from "./pages/Login";
-// import VerifyOtp from "./pages/VerifyOtp";
-// import Home from "./pages/Home";
-// import Layout from "./components/Layout";
-// import JournalEntry from "./pages/JournalEntry";
-// import Analytics from "./pages/Analytics";
-// import {ThemeProvider} from "./context/ThemeContext"
-// import AIChat from "./pages/AIChat";
 
-// function App() {
-//   return (
-//     <ThemeProvider>
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<Layout />}>
-//           <Route index element={<Home />} />
-//           <Route path="/signup" element={<Signup />} />
-//           <Route path="/login" element={<Login />} />
-//           <Route path="/verify-otp" element={<VerifyOtp />} />
-//           <Route path="/journal" element={<JournalEntry/>}/>
-//           <Route path="/chat" element={<AIChat />} />
-//           <Route path="/analytics" element={<Analytics />} />
-//         </Route>
-//       </Routes>
-//     </Router>
-//     </ThemeProvider>
-//   );
-// }
-
-// export default App;
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
@@ -41,6 +10,8 @@ import Analytics from "./pages/Analytics";
 import { ThemeProvider } from "./context/ThemeContext";
 import AIChat from "./pages/AIChat";
 import PrivateRoute from "./components/PrivateRoute"; // ✅ add this
+import AdminDashboard from "./admin/AdminDashboard";
+import AdminRoute from "./admin/AdminRoute";
 
 function App() {
   return (
@@ -79,6 +50,16 @@ function App() {
               }
             />
           </Route>
+          <Route
+          path="/admin"
+          element={
+         
+            <AdminRoute>
+              <AdminDashboard />
+            </AdminRoute>
+           
+          }
+        />
         </Routes>
       </Router>
     </ThemeProvider>
