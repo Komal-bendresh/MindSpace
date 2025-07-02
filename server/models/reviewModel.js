cc672f43262036ae7f2977d18f0bb6004fcadc7f
komal-bendresh <komalbendresh1134@gmail.com>
2025-07-02 09:50:48 +0530

    added unique true for user to get one rating and review per user

--- server/models/reviewModel.js
+++ server/models/reviewModel.js
@@ -4,7 +4,8 @@ const reviewSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
-    required: true
+    required: true,
+     unique: true
   },
   rating: {
     type: Number,
