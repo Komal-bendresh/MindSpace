cab8d019a9ea590a213ac7ba5954f2d6b2c661dc
komal-bendresh <komalbendresh1134@gmail.com>
2025-07-02 09:08:43 +0530

    wrapped app in AuthProvider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,13 +5,14 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import {ToastContainer} from 'react-toastify'
 import {Toaster} from 'react-hot-toast'
+import { AuthProvider } from "./context/AuthContext.jsx";
 
 createRoot(document.getElementById('root')).render(
 
      
-  
+    <AuthProvider>
          <App />
- 
+  </AuthProvider>
 
   
 )
