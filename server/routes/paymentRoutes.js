6d134b51a3ee88b2ba4fad5681c0042c2fa1b5a4
komal-bendresh <komalbendresh1134@gmail.com>
2025-07-02 09:53:46 +0530

    deleted

--- server/routes/paymentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { createOrder, verifyPayment } = require("../controllers/paymentController");
-const authMiddleware = require("../middleware/authMiddleware");
-
-router.post("/create-order", authMiddleware, createOrder);
-router.post("/verify", authMiddleware, verifyPayment);
-
-module.exports = router;
